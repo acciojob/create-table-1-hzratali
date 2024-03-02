7940d07c311a693bba0d0fa5ae9b70c6e6fcb9a1
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-03 01:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function insert_Row() {
 	var newRow = document.createElement('tr');
     var cell1 = document.createElement('td');
     var cell2 = document.createElement('td');
-    cell1.innerHTML = ''; // Clear existing content
+    cell1.innerHTML = 'New Cell1';
     cell2.innerHTML = 'New Cell2';
     newRow.appendChild(cell1);
     newRow.appendChild(cell2);
     var table = document.getElementById('sampleTable');
-    table.prepend(newRow);
+	table.prepend(newRow);
 }
