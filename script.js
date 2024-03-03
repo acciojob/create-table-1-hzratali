d1f31d7c1f21e67cb6b0adc35537a73cd768a5b6
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-03 15:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function insert_Row() {
     var cell2 = document.createElement('td');
     cell1.innerHTML = 'New Cell1';
     cell2.innerHTML = 'New Cell2';
-    newRow.appendChild(cell1);
-    newRow.appendChild(cell2);
     var table = document.getElementById('sampleTable');
-	table.prepend(newRow);
+	var newRow = table.insertRow(0);
+	newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
 }
